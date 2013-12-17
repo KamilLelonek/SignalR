a72d79362e2a7ef0d23dae5f945192875ce85e54
squixy <squixy.sln@gmail.com>
2013-12-17 18:53:10 +0100

    Initial commit

---
+++ PSW/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using PSW.Models;
+
+namespace PSW.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Game()
+        {
+            return View();
+        }
+    }
+}
